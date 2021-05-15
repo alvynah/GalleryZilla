1f49204d581e128d612e6e98239045e2fdd27014
alvynah <alvynahw@gmail.com>
2021-05-15 11:15:16 +0300

    Test save location successfully

--- picture/models.py
+++ picture/models.py
@@ -4,10 +4,28 @@ from django.db import models
 class Location (models.Model):
     location=models.CharField(max_length =100)
 
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.save_location()
+        self.delete()
+
+    def __str__(self):
+        return self.location
+
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category
+
 class Picture(models.Model):
-    image=models.ImageField(upload_to='pictures/')
     title =models.CharField(max_length = 100)
     description= models.TextField()
     posted_at=models.DateTimeField(auto_now_add=True)
-    location=models.ForeignKey('Location',on_delete=models.CASCADE,default='Nairobi')
+    location=models.ForeignKey('Location',on_delete=models.CASCADE)
+    category=models.ForeignKey('Category',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
--- picture/tests.py
+++ picture/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Picture,Category,Location 
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.kenya=Location(location='Kenya')
+    # Testing_instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kenya,Location))
+    # Testing Save Method
+    def test_save_method(self):
+        self.kenya.save_location()
+        locations=Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
