c5db1711442a8ab8ddc04876751b6e2887fbc702
alvynah <alvynahw@gmail.com>
2021-05-15 10:38:26 +0300

    Add table Location and a foriegn key for location in picture

--- picture/models.py
+++ picture/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Location (models.Model):
+    location=models.CharField(max_length =100)
 
 class Picture(models.Model):
     image=models.ImageField(upload_to='pictures/')
     title =models.CharField(max_length = 100)
     description= models.TextField()
     posted_at=models.DateTimeField(auto_now_add=True)
+    location=models.ForeignKey('Location',on_delete=models.CASCADE,default='Nairobi')
+
