d2aa8984a322f703926a5f40ce6e8a9c94b905af
alvynah <alvynahw@gmail.com>
2021-05-15 13:23:41 +0300

    Add location and category foreign keys to table picture

--- picture/models.py
+++ picture/models.py
@@ -42,7 +42,10 @@ class Picture(models.Model):
     image=models.ImageField(upload_to='pictures/')
     title =models.CharField(max_length = 100)
     description= models.TextField()
+    location= models.ForeignKey('Location',on_delete=models.CASCADE)
+    category= models.ForeignKey('Category',on_delete=models.CASCADE)
     posted_at=models.DateTimeField(auto_now_add=True)
+
 
 
     def __str__(self):
