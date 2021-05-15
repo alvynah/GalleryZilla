47bd63e2423cef406f47999ac3b6d78ec784865c
alvynah <alvynahw@gmail.com>
2021-05-15 09:11:06 +0300

    Add bootstrap and javascript links to base.html

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'picture.apps.PictureConfig',
+    'picture',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
