869ad6e59f8128749a51f1b38b143c2366c50a88
alvynah <alvynahw@gmail.com>
2021-05-15 10:22:48 +0300

    Add model picture and update migrations

--- picture/models.py
+++ picture/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Picture(models.Model):
+    image=models.ImageField(upload_to='pictures/')
+    title =models.CharField(max_length = 100)
+    description= models.TextField()
+    posted_at=models.DateTimeField(auto_now_add=True)
