39c7473e5d458145041d8607b96e0b00817095cb
alvynah <alvynahw@gmail.com>
2021-05-15 13:07:28 +0300

    Virtual

--- picture/admin.py
+++ picture/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Picture,Category,Location
+# from .models import Picture,Category,Location
 
-# Register your models here.
-admin.site.register(Picture)
-admin.site.register(Category)
-admin.site.register(Location)
+# # Register your models here.
+# admin.site.register(Picture)
+# admin.site.register(Category)
+# admin.site.register(Location)
