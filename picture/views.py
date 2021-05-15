16a71390afcd0a88da003d4179ed3deddb5b22d1
alvynah <alvynahw@gmail.com>
2021-05-15 17:19:56 +0300

    Display images added

--- picture/views.py
+++ picture/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,HttpResponse
+from .models import Picture,Location,Category
 
 # Create your views here.
 def welcome(request):
-    return render(request,'index.html')
+    pictures = Picture.objects.all()
+    return render(request,'all-pictures/index.html',{'pictures':pictures})
 
